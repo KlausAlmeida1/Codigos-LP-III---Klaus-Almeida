ada6c4b790b2101cfc1f377d73606e16d7d25081
Klaus Almeida <74985395+ttercept2@users.noreply.github.com>
2022-10-26 15:48:40 -0300

    Add files via upload

---
+++ ClasseData/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {        
+       Data2 data1 = new Data2();
+       data1.setInicializarData(20, 12, 2022);
+       data1.getImprimirdataExtenso();
+       System.out.println();
+       data1.getImprimirdata();
+    }
+}
