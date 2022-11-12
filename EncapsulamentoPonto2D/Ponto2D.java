46bd81eee73f948e8cb22ad2cbfb4abc8b1af19a
Klaus Almeida <74985395+ttercept2@users.noreply.github.com>
2022-11-12 15:10:13 -0300

    Add files via upload

---
+++ EncapsulamentoPonto2D/Ponto2D.java
@@ -0,0 +1,79 @@
+public class Ponto2D {
+    private float x,y;
+
+    public Ponto2D(float x,float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Ponto2D(){
+        this(0,0);
+    }
+
+
+    public float getX(){
+        return this.x;
+    }
+
+    public float getY(){
+        return this.y;
+    }
+
+    public boolean isEixoX(){
+        if(this.y==0 && this.x!=0){
+            return true;
+        }
+        return false;
+        
+    }
+
+    public boolean isEixoY(){
+        if(this.x==0 && this.y!=0){
+            return true;
+        }
+        return false;  
+    }
+
+    public boolean isEixos(){
+        if(this.x==0 && this.y==0){
+            return true;
+        }
+        return false;
+    }
+
+    public int quadrante(){
+        if(isEixos()||isEixoX()||isEixoY()){
+            return 0;
+        }
+        if(this.y>0&&this.x>0){
+            return 1;
+        }
+        if(this.y>0&&this.x<0){
+            return 2;
+        }
+        if(this.y<0&&this.x<0){
+            return 3;
+        }
+        return 4;
+    }
+
+    public void imprimirPonto(){
+        System.out.printf("Ponto x:%f\nPonto y:%f\n",x,y);
+    }
+
+    public double distEuclidiana(Ponto2D ponto){
+        float difX = this.x - ponto.x;
+        float difY = this.y - ponto.y;
+        return Math.sqrt((difX)*(difX)+(difY)*(difY));
+    }
+
+    public double distEuclidiana(float x,float y){
+        float difX = this.x - x;
+        float difY = this.y - y;
+        return Math.sqrt((difX)*(difX)+(difY)*(difY));
+    }
+
+    public double distEuclidiana(){
+        return this.distEuclidiana(0,0);
+    }
+}
