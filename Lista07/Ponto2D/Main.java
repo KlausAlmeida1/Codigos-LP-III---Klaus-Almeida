06a36a79b69ac1fa075e51b74196f576636dee2b
Klaus Almeida <74985395+ttercept2@users.noreply.github.com>
2022-12-05 19:15:06 -0300

    Add files via upload

--- Lista07/Ponto2D/Main.java
+++ Lista07/Ponto2D/Main.java
@@ -7,8 +7,14 @@ public class Main{
         Ponto2D ponto = new Ponto2D(x,y);
 
         Ponto3DAgregação segundoPonto = new Ponto3DAgregação(ponto,z);
-
+        Ponto3DHerança terceiroPonto = new Ponto3DHerança(2,50,5);
         System.out.println(segundoPonto.toString());
         System.out.println(segundoPonto.distance(1,2,3));
+        System.out.println("HERANÇA:");
+        System.out.println(terceiroPonto.toString());
+        terceiroPonto.imprimirPonto();
+
+
+
     }
 }
---
+++ Lista07/Ponto2D/Ponto3DHerança.java
@@ -0,0 +1,65 @@
+public class Ponto3DHerança extends Ponto2D {
+    float z;
+    public Ponto3DHerança(float x,float y,float z){
+        super(x,y);
+        this.z = z;
+
+    }
+    public String toString(){
+        return super.toString()+" z:"+Float.toString(this.z);
+    }
+    
+    public float getZ(){
+        return this.z;
+    }
+
+    public boolean inAxisX(){
+        if(super.inAxisX()== true && this.z!=0){
+            return true;
+        }
+        return false;
+        
+    }
+
+    public boolean inAxisY(){
+        if(super.inAxisY()== true && this.z!=0){
+            return true;
+        }
+        return false;  
+    }
+    public boolean inAxisZ(){
+        if(super.inAxis()== true && this.z==0){
+            return true;
+        }
+        return false;  
+    }
+
+    public boolean inAxis(){
+        if(super.inAxis()== true && this.z!=0){
+            return true;
+        }
+        return false;  
+    }
+
+    public boolean isOrigin(){
+        if(super.isOrigin() && this.z!=0){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void imprimirPonto(){
+        System.out.printf("Ponto x: %f\nPonto y: %f \nPonto z: %f",this.getX(),this.getY(),this.z);
+    }
+
+    public double distance(float x,float y,float z){
+        float difX = this.getX() - x;
+        float difY = this.getY() - y;
+        float difZ = this.z - z;
+        return Math.sqrt((difX)*(difX)+(difY)*(difY)+(difZ)*(difZ));
+    }
+
+    public double distance(){
+        return this.distance(0,0,0);
+    }
+}
